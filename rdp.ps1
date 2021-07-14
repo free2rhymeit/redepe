9d3c546a23f781c67383b0f36910afd3f6880e40
anstwn <86707160+anstwn@users.noreply.github.com>
2021-07-14 15:01:32 +0700

    Update rdp.ps1

--- rdp.ps1
+++ rdp.ps1
@@ -7,6 +7,6 @@ Copy ngrok.exe C:\Windows\System32
 Start-Service -Name audiosrv
 cmd /c echo ./ngrok.exe authtoken "1tyZf7iesdfYzLI6wAaRWCFFIqI_2eUqMnB8b5m9vbobYeMpp" >a.ps1
 cmd /c echo cmd /k start ngrok.exe tcp 3389 >>a.ps1
-cmd /c echo ping -n 999999 10.10.10.10 >>a.ps1
+cmd /c echo ping -n 999999 8.8.8.8 >>a.ps1
 cmd /c log.bat
 .\a.ps1
